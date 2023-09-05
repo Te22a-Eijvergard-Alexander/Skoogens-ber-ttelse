6f0b8d9303f06479e20896b2f5194ba0e4bf7a27
Alexander Eijvergard <alexander.eijvergard@elev.ga.ntig.se>
2023-09-05 11:10:56 +0200

    Påbörjat min berättelse

---
+++ Snövits berättelse/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("Vad heter du?");
+
+string name = Console.ReadLine().ToLower();
+
+if (name =="Alex" || name == "alex")
+{
+    Console.WriteLine ("Welcom!");
+    
+}
+
+else if (name == "Skoogen" || name == "skoogen")
+{
+    Console.WriteLine("King kong");
+}
+
+else
+{
+    Console.WriteLine("KYS (Keep yourself safe)");
+}
+
+//if (name != "Alex")
+
+Console.ReadLine();
+
+
